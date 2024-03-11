138f0ad7f83af1f1897e59e0b02a72ef183282ed
yuuya-hoshino <sjlthmfjx@icloud.com>
2024-03-11 16:55:40 +0900

    react作成

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.Text}>Islamと一緒にご飯を食べる細江</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FF00FF',
     alignItems: 'center',
     justifyContent: 'center',
   },
